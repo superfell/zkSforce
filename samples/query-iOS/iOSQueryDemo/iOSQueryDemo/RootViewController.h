b3cdaf0bb564d63ab2149849d494d924c75d2ce4
Simon Fell <fellforce@gmail.com>
2011-04-24 11:03:27 -0700

    Initial shell of ios query sample

---
+++ samples/query-iOS/iOSQueryDemo/iOSQueryDemo/RootViewController.h
@@ -0,0 +1,15 @@
+//
+//  RootViewController.h
+//  iOSQueryDemo
+//
+//  Created by Simon Fell on 4/24/11.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController {
+
+}
+
+
+@end
---
+++ samples/query-iOS/iOSQueryDemo/iOSQueryDemo/iOSQueryDemoAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  iOSQueryDemoAppDelegate.h
+//  iOSQueryDemo
+//
+//  Created by Simon Fell on 4/24/11.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface iOSQueryDemoAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
