d0268a6f4fe779def7a0114450ffdb6c54e22e25
Simon Fell <fellforce@gmail.com>
2011-05-22 22:18:38 -0700

    add missing retain

--- zkSforce/zkSforceClient.m
+++ zkSforce/zkSforceClient.m
@@ -147,7 +147,7 @@ static const int SAVE_BATCH_SIZE = 25;
 
 - (ZKUserInfo *)currentUserInfo {
     if (userInfo == nil)
-        userInfo = [self getUserInfo];
+        userInfo = [[self getUserInfo] retain];
 	return userInfo;
 }
 
