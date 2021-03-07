adba1d7b2fdea8dca279ad35995b2523a007be23
Simon Fell <fellforce@gmail.com>
2021-03-07 13:44:19 -0800

    remove duplicated forward decls

--- zkSforce/generated/ZKLeadConvert.h
+++ zkSforce/generated/ZKLeadConvert.h
@@ -27,9 +27,6 @@
 #import "ZKXMLSerializable.h"
 
 @class ZKSObject;
-@class ZKSObject;
-@class ZKSObject;
-@class ZKSObject;
 /*
 <complexType name="LeadConvert" xmlns="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/wsdl/soap/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:fns="urn:fault.partner.soap.sforce.com" xmlns:tns="urn:partner.soap.sforce.com" xmlns:ens="urn:sobject.partner.soap.sforce.com">
   <sequence>
